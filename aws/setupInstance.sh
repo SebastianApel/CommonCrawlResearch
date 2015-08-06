6f7ce87346c2ceddaa2b81abc16c15053a9e3131
SebastianApel <seapgitbug@pelletsdirekt.com>
2015-08-06 12:17:57 +0200

    changed CR/LF problems

--- aws/setupInstance.sh
+++ aws/setupInstance.sh
@@ -4,7 +4,10 @@
 
 
 # Get the scripts from the repository
+cd ~/
+rm -rf CommonCrawlResearch
 wget https://github.com/SebastianApel/CommonCrawlResearch/archive/master.zip
 unzip -o master
 rm master.zip
 mv CommonCrawlResearch-master/ CommonCrawlResearch
+sudo chmod -R a+rx CommonCrawlResearch # quick fix for permissions problems 
