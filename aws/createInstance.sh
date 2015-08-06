ad4d2c0db119f1f4c485e9076e57f83e7725ef43
SebastianApel <seapgitbug@pelletsdirekt.com>
2015-08-06 11:26:00 +0200

    Added CreateInstance script

---
+++ aws/createInstance.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Helper script for aws
+#
+# - Starts instance
+# - Waits
+# - SSHs to it
+#
+#
+
+# Start instance
+aws ec2 run-instances --image-id ami-a8221fb5 --count 1 --instance-type t2.micro --key-name MyKeyPair --security-groups SecurityGroup1
+
+# Wait until machine has started
+sleep 30
+
+# Query the public IP address of the instance
+IPADDR=`aws ec2 describe-instances | grep PublicIpAddress | awk 'BEGIN{FS="\""}{print $4}'`
+echo $IPADDR
+
+# And login via ssh
+ssh -i MyKeyPair.pem ec2-user@$IPADDR
